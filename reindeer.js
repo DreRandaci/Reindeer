8e9a562b9c5cb4acb55c62856e1e97a182e276e9
Dre mf Randaci <drerandaci@gmail.com>
2017-07-26 17:30:57 -0500

    finished this silly exercise and gained a nice headache

--- reindeer.js
+++ reindeer.js
@@ -5,11 +5,18 @@
 // 	Red Dancer
 
 
-
+console.log("This is the beginning of my js file")
 
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
 
 var hohohoElement = document.getElementById("reindeer");
+var html = "";
 
+debugger;
+for(var i = 0; i < reindeer.length; i++) {
+    html += '<p>' + colors[i] + ' ' + reindeer[i] + '</p>';
+};
+
+hohohoElement.innerHTML = html;
